5c4bef4acf3f223fcc8198b625d4a5e5dd923ef5
JonathanDavid630 <pb0797032019@unab.edu.sv>
2021-10-16 17:37:57 -0600

    Servicio
    Clase creada

---
+++ LaboDos/Servicio/IPersona.cs
@@ -0,0 +1,15 @@
+﻿using LaboDos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LaboDos.Servicio
+{
+    interface IPersona
+    {
+        void Save(Persona c);
+
+        List<Persona> ListDataPersona();
+    }
+}
