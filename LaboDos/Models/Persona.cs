2755e2d91fa429e1d14bed85acfec8f7c927dbfc
JonathanDavid630 <pb0797032019@unab.edu.sv>
2021-10-19 11:20:56 -0600

    Actualizacion
    Nombres

--- LaboDos/Models/Persona.cs
+++ LaboDos/Models/Persona.cs
@@ -21,9 +21,10 @@ namespace LaboDos.Models
         [Required(ErrorMessage = "estos campos son requeridos")]
         public string NombrePersona { get; set; }
 
-        [Range(18, 100, ErrorMessage ="El campo {0} debe estar entre los números {1} y {2}")]
+
         [Display(Name = "Edad")]
         [Required(ErrorMessage = "estos campos son requeridos")]
+        [Range(18, 100, ErrorMessage = "El campo {0} debe estar entre los números {1} y {2}")]
         public Nullable<int> EdadPersona { get; set; }
 
         [Display(Name = "DescripcionPersona ")]
